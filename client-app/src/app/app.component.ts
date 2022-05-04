9e6ade60e50b59b4ee31c6f3d3546ac5268bfdad
onionmccabbage <onionmccabbage@live.com>
2022-05-04 11:01:58 +0100

    Add files via upload

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -7,18 +7,18 @@ import { UsersService } from './users.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // use the constructor to instantiate our service
+  // use the constructor to instantiate our service singleton
   constructor(private userService:UsersService){}
 
   // properties of this component
-  users
+  users:Array<object> // typescipt lets us specify the expected data type
   //methods of this component
   ngOnInit(): void {
       // make a call to the service as soon as this component is initialized
       this.callUserService()
   }
   callUserService(){
-    // here we can make a cal to our UsersService instance
+    // here we can make a call to our UsersService instance
     this.userService.getUsers() // this returns an Observable
       // we subscribe to the observable - we are notified of EVERY response
       .subscribe( (users)=>{this.users = users} ) // this is async - we wait
